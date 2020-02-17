84493153f64e9e122a4aef6dcde879c582fed326
Isaac Lewis <isaacwlewis@gmail.com>
2020-02-17 00:22:21 -0600

    Updates

---
+++ precache-manifest.78ec4d319033c771ccf36f8b57f0b03c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ab0c0c2996c37e5872989adc340bed",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "19f67135b0beec0c9a0b",
+    "url": "/my-site/static/css/main.d2a65b88.chunk.css"
+  },
+  {
+    "revision": "fd40e00896b01d117975",
+    "url": "/my-site/static/js/2.ecb590fd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-site/static/js/2.ecb590fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19f67135b0beec0c9a0b",
+    "url": "/my-site/static/js/main.994febfb.chunk.js"
+  },
+  {
+    "revision": "839b4f1ffb4334eb1491",
+    "url": "/my-site/static/js/runtime-main.1b6100af.js"
+  },
+  {
+    "revision": "b1b630e16e5eedb822f0df26f766c785",
+    "url": "/my-site/static/media/book.b1b630e1.jpg"
+  },
+  {
+    "revision": "f0104f5e796e6c81975c82592ed8a1ab",
+    "url": "/my-site/static/media/calc.f0104f5e.jpg"
+  }
+]);
